025126a0f0df754ff0d26771733a3c52f7f2b227
JacRay <64971844+JacRay@users.noreply.github.com>
2021-08-30 21:43:44 +0530

    Argument

---
+++ Argument.rb
@@ -0,0 +1,6 @@
+first, second, third = ARGV
+#ARGV unpacks the argument
+# use Firstone = ARGV.first to get specific argument
+puts "Your First argument is #{first}"
+puts "Your Second argument is #{second}"
+puts "Your Third argument is #{third}"
