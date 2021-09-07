50c9e3ae0d45d916409e154419fab35fda8d5715
JacRay <64971844+JacRay@users.noreply.github.com>
2021-09-07 17:04:40 +0530

    Here Document

---
+++ heredoc.rb
@@ -0,0 +1,16 @@
+#here << is called here doc we can use any captital word than END
+poem = <<END 
+\t Heloo 
+How are you 
+This is me
+END
+
+# Example NEW
+words = <<NEW
+Hai all
+This is ruby
+NEW
+
+puts poem
+puts words
+
