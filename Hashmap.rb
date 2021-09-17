e02ffc3992e247b93c332d548926825ed3a6097d
JacRay <64971844+JacRay@users.noreply.github.com>
2021-09-17 20:20:06 +0530

    HashMap

--- Hashmap.rb
+++ Hashmap.rb
@@ -1,1 +1,8 @@
 #Hashmaps are like dictionary in python
+stuff = {'name' => 'Zed', 'age' => 39, 'height' => 6 * 12 + 2}
+{"name"=>"Zed", "age"=>39, "height"=>74}
+puts stuff['name']
+puts stuff['age']
+puts stuff['height']
+stuff['city'] = "San Francisco"
+print stuff['city']
