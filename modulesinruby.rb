0fe031fd1294690ae563a45d6d52d0d48f84ae3d
JacRay <64971844+JacRay@users.noreply.github.com>
2021-10-07 20:17:22 +0530

    Modules

---
+++ modulesinruby.rb
@@ -0,0 +1,14 @@
+module MyStuff
+    def MyStuff.apple()
+        puts "I AM APPLES!"
+    end
+
+    # this is just a variable
+    TANGERINE = "Living reflection of a dream"
+end
+
+#Can use this module in another file Like this:
+# require "./mystuff.rb"
+
+# MyStuff.apple()
+# puts MyStuff::TANGERINE
